ee74b4da1ba9af3473c23ad925532aca4715e8a2
Your Name <you@example.com>
2024-09-15 21:44:43 +0200

    Corrected module paths

--- nn_onnx/model/onnx_to_tensorrt.py
+++ nn_onnx/model/onnx_to_tensorrt.py
@@ -3,8 +3,8 @@ import onnx
 from pynnlib.import_libs import is_tensorrt_available
 from pynnlib.model import OnnxModel
 if is_tensorrt_available():
-    from pynnlib.tensor_rt.trt_types import ShapeStrategy, TensorrtModel
-    from pynnlib.tensor_rt.model.onnx_to_trt import onnx_to_trt_engine
+    from pynnlib.nn_tensor_rt.trt_types import ShapeStrategy, TensorrtModel
+    from pynnlib.nn_tensor_rt.model.onnx_to_trt import onnx_to_trt_engine
 else:
     # print("[W] TensorRT is not supported: model cannot be converted")
     def onnx_to_trt_engine(*args):
