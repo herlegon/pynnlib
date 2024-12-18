c30f18c015ccaf7db80ad8b49b1aff4094f371db
Arnaud de Gérard <arnaud.degerard@gmail.com>
2024-12-18 17:54:50 +0100

    back

--- utils/tensor.py
+++ utils/tensor.py
@@ -7,12 +7,12 @@ if is_cuda_available():
         to_nchw,
         to_hwc,
     )
-# else:
-#     from .torch_tensor import (
-#         flip_r_b_channels,
-#         to_nchw,
-#         to_hwc,
-#     )
+else:
+    from .torch_tensor import (
+        flip_r_b_channels_torch as flip_r_b_channels,
+        to_nchw_torch as to_nchw,
+        to_hwc_torch as to_hwc,
+    )
 
-#     class MemcpyKind:
-#         pass
+    class MemcpyKind:
+        pass
